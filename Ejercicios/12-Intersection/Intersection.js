b17c2c615ef40cdbc3d35e53c59bc7c027226f3a
marcos <marcos.vera@fizzmod.com>
2021-09-02 13:14:19 -0300

    funcion para repetir con sus test

--- Ejercicios/12-Intersection/Intersection.js
+++ Ejercicios/12-Intersection/Intersection.js
@@ -1,6 +1,7 @@
 function intersection(arr1, arr2) {
   // Your code here:
 
+  return arr1.filter( e => arr2.includes(e));
 }
 
 module.exports = intersection;
--- Test/12-Intersection.spec.js
+++ Test/12-Intersection.spec.js
@@ -1,6 +1,29 @@
 const { expect } = require("chai");
 const intersection = require("../Ejercicios/12-Intersection/Intersection");
 
+const arr1 = [1,3,5,7,10];
+const arr2 = [2,3,6,8,10,20];
+
 describe("Intersection", function() {
 
+	it('When you find the same', () => {
+
+		const result = intersection(arr1, arr2);
+
+		expect(result).to.deep.equal([3, 10]);
+	});
+
+	it('When an empty array is received', () => {
+
+		const result = intersection([], arr2);
+
+		expect(result).to.deep.equal([]);
+	});
+
+	it('When there are no repeats', () => {
+
+		const result = intersection([33,44,55], arr2);
+
+		expect(result).to.deep.equal([]);
+	});
 });
